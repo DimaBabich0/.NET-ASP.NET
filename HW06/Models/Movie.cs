e6b20b15df056255f7edc0c0e329514033c33a32
Dima Babich <dimavernigor7@gmail.com>
2025-03-23 04:55:28 +0200

    Uploaded HW06

---
+++ HW06/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HW03.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [Display(Name = "Director")]
+        public string? DirectorsName { get; set; }
+        public string? Genre { get; set; }
+        public int Year { get; set; }
+        [Display(Name = "Poster file")]
+        public string? PosterPath { get; set; }
+        public string? Synopsis { get; set; }
+    }
+}
